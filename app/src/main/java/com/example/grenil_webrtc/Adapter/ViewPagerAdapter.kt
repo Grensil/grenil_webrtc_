ebc5d16f3daefeb6ba0da256f12336e26bd9f5ad
grensil <grensil@willm.me>
2022-02-17 10:34:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/grenil_webrtc/Adapter/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.grenil_webrtc.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(manager: FragmentActivity) : FragmentStateAdapter(manager) {
+    //var fragments = listOf<Fragment>()
+    private var fragments : ArrayList<Fragment> = ArrayList()
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyItemInserted(fragments.size-1)
+    }
+
+}

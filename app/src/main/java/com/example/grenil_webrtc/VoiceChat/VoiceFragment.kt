a8e43dc4b6e9e798ec209ae89047d000ba1ed601
정동준 <grensil@willim.me>
2022-02-23 15:33:11 +0900

    edit

--- app/src/main/java/com/example/grenil_webrtc/VoiceChat/VoiceFragment.kt
+++ app/src/main/java/com/example/grenil_webrtc/VoiceChat/VoiceFragment.kt
@@ -54,9 +54,6 @@ class VoiceFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-
-
     }
 
     override fun onCreateView(
